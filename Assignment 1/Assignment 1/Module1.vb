0ca027ccb934515b3b4c17354e649e3288713ebc
uwi50 <78470739+uwi50@users.noreply.github.com>
2022-07-06 20:55:35 +0200

    Add files via upload

---
+++ Assignment 1/Assignment 1/Module1.vb
@@ -0,0 +1,57 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim j As Integer
+        Dim NameList As List(Of String) = New List(Of String)
+        Dim OOPList As List(Of Decimal) = New List(Of Decimal)
+        Dim JavaList As List(Of Decimal) = New List(Of Decimal)
+        Dim CList As List(Of Decimal) = New List(Of Decimal)
+        Dim StudName As String
+        Dim OOP, Java, C As Decimal
+        Dim AvgOOP, AvgJava, AvgC As Decimal
+        AvgOOP = 0
+        AvgJava = 0
+        AvgC = 0
+        Do
+            Console.Write("Enter student name:")
+            StudName = Console.ReadLine()
+
+            If StudName <> LCase("N") Then
+                NameList.Add(StudName)
+                Console.Write(StudName & " OOP Grade:")
+                OOP = Console.ReadLine()
+                OOPList.Add(OOP)
+                AvgOOP = AvgOOP + OOP
+                Console.Write(StudName & " Java Grade:")
+                Java = Console.ReadLine()
+                JavaList.Add(Java)
+                AvgJava = AvgJava + Java
+                Console.Write(StudName & " C Grade:")
+                C = Console.ReadLine()
+                CList.Add(C)
+                AvgC = AvgC + C
+
+            End If
+            Console.WriteLine(" ")
+        Loop While StudName <> LCase("N")
+
+        Console.WriteLine(" ")
+        Console.WriteLine(" ")
+        Console.WriteLine("List of students with their respective Grades:")
+        Console.WriteLine("  ")
+        Console.WriteLine("-----------------------------------")
+        Console.WriteLine("  Student_Name     OOP   Java   C")
+        Console.WriteLine("-----------------------------------")
+        Console.WriteLine("  ")
+        For j = 0 To NameList.Count - 1
+            Console.WriteLine(j + 1 & ". " & NameList(j) & "         " & OOPList(j) & "     " & JavaList(j) & "    " & CList(j))
+            Console.WriteLine(" ")
+        Next
+        Console.WriteLine(" ")
+        Console.WriteLine("-----------------------------------")
+        Console.WriteLine("  Average          " & AvgOOP / NameList.Count & "     " & AvgJava / NameList.Count & "     " & AvgC / NameList.Count)
+        Console.WriteLine("-----------------------------------")
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ Assignment 1/Assignment 1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Assignment 1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Assignment 1")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6d07a696-de99-4a22-817a-da0150292799")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
